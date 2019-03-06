9c37a002fb0262ab0183835d342b4f3969fc6b40
Corban10 <Corban30303@hotmail.com>
2019-03-07 12:39:50 +1300

    get ratings

--- mal.py
+++ mal.py
@@ -44,27 +44,49 @@ api = Jikan()
 
 #search = pd.read_csv("anime_valid.csv")
 
-club = []
-member_list = []
-def get_club_members( y ):
+#club = []
+#member_list = []
+#def get_club_members( y ):
+#    try:
+#        club = api.club(y, 'members')
+#        for x in range(0, 35):
+#            if not club['members'][x]['username'] in member_list:
+#                member_list.append(club['members'][x]['username'])
+#    except:
+#        e = sys.exc_info()[0]
+#
+#for y in range(1, 1001):
+#    get_club_members(y)
+#    time.sleep(2)
+#
+#username_df = pd.DataFrame(data=member_list, columns=['username'])
+#username_df.to_csv("users.csv", encoding='utf-8', index=False)
+
+## ------------------------------------------------------------------------------
+## get list of rating + mal_id from each user
+## ------------------------------------------------------------------------------
+
+search = pd.read_csv("users.csv")
+ratings = []
+ratings_list = []
+
+def get_ratings(y):
     try:
-        club = api.club(y, 'members')
-        for x in range(0, 35):
-            if not club['members'][x]['username'] in member_list:
-                member_list.append(club['members'][x]['username'])
+        ratings = api.user(username=y, request='animelist', argument='completed', page=1)
+        for x in range(0, 200):
+            ratings_list.append([y, ratings['anime'][x]['mal_id'], ratings['anime'][x]['score']])
     except:
         e = sys.exc_info()[0]
 
-for y in range(1, 2):
-    get_club_members(y)
+for i in range(0, 600): #300
+    get_ratings(search.iloc[i]['username'])
     time.sleep(2)
 
-username_df = pd.DataFrame(data=member_list, columns=['username'])
-username_df.to_csv("users.csv", encoding='utf-8', index=False)
-print(username_df.head())
+ratings_df = pd.DataFrame(data=ratings_list, columns=['username', 'mal_id', 'score'])
+ratings_df.to_csv("ratings.csv", encoding='utf-8', index=False)
 
 ## ------------------------------------------------------------------------------
-## get completed anime amount from user
+## get completed anime amount from user ?maybe?
 ## ------------------------------------------------------------------------------
 
 #user = []
@@ -81,26 +103,3 @@ print(username_df.head())
 #user_df = pd.DataFrame(data=user_list, columns=['username', 'completed']) 
 #print(anime_df.head())
 #top_anime_df.to_csv("anime.csv", encoding='utf-8', index=False)
-
-## ------------------------------------------------------------------------------
-## get list of rating + mal_id from each user
-## ------------------------------------------------------------------------------
-
-#rating = []
-#rating_list = []
-#def get_ratings( y ):
-#    for x in range(0, 35):
-#        try:
-#            club = api.club(y, 'members')
-#            member_list.append(club['members'][x]['username'])
-#            time.sleep(2)
-#        except:
-#            e = sys.exc_info()[0]
-#for y in range(1, 300):
-#    get_club_members( y )
-#username_df = pd.DataFrame(data=member_list, columns=['username'])
-#print(username_df)
-
-#https://api.jikan.moe/v3/user/emfcmkh/animelist/completed
-
-
